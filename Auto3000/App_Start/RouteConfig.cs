806e47d2ff122ca4ea530ae9bfadd627cfb6c22c
Cyber Croc <cybercrocindia@gmail.com>
2013-05-15 11:21:20 +0530

    Commit For Demo (Anuj/Gurpreet)

---
+++ Auto3000/App_Start/RouteConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Auto3000
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+
+            routes.MapRoute("AdminLogin", "admin/login", new { controller = "AdminAccount", action = "Login" });
+            routes.MapRoute("AdminAnnouncement", "admin/announcements", new { controller = "AdminAnnouncement", action = "Index" });
+            routes.MapRoute("AdminAppointmentDelete", "admin/announcementdel", new { controller = "AdminAnnouncement", action = "Delete" });
+            routes.MapRoute("AdminDashboard", "admin/dashboard", new { controller = "AdminAccount", action = "Dashboard" });
+
+
+
+            #region [Routes For FeedBack Admin]
+            routes.MapRoute("AdminFeedback", "admin/feedback", new { controller = "AdminFeedback", action = "Index" });
+            routes.MapRoute("AdminFeedbackDelete", "admin/feedback", new { controller = "AdminFeedback", action = "Delete" });
+            #endregion
+
+            #region [Routes For Contact Admin]
+            routes.MapRoute("AdminContact", "admin/contact", new { controller = "AdminContact", action = "Index" });
+            routes.MapRoute("AdminContactDelete", "admin/contact", new { controller = "AdminContact", action = "Delete" });
+            #endregion
+
+        
+        }
+    }
+}
---
+++ Auto3000/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Auto3000.Models;
+
+namespace Auto3000.Controllers
+{
+    /*Base Controller for all Default Functions*/
+    public class BaseController : Controller
+    {
+        public Entities dbcontext = new Entities();
+
+        public void CheckLogin()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserId"])))
+            {
+                Response.RedirectToRoute("AdminLogin", "admin/login");
+            }
+        }
+    }
+}
